87a162686244df352a01b91806c8aa7893ec5606
Martin Faucheux <martin.faucheux@gmail.com>
2024-06-06 18:01:37 +0200

    add gh token

--- some_script.py
+++ some_script.py
@@ -4,4 +4,6 @@ domain = "martinfaucheux.testdomain.com"
 
 domain = "martinfaucheux.testotherdomain.com"
 
+GITHUB_TOKEN = "0d60b5e9856b9b2999cf7a08bf742a303b158aa4"
+
 # test stuff for gitguardian.com
