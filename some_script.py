d74eb776a92a2bb8928595eae35772d68fda33b1
Martin Faucheux <martin.faucheux@gmail.com>
2024-10-16 17:17:52 +0200

    Update some_script.py

--- some_script.py
+++ some_script.py
@@ -7,3 +7,4 @@ domain = "martinfaucheux.testotherdomain.com"
 GITHUB_TOKEN = "0d66b5ed156b0b2999cf7a18bf74fa303b158aa4"
 
 # test stuff for gitguardian.com
+GITLAB_TOKEN = "glpat-cTxjRuur1Sri3Qh2iFfx"
